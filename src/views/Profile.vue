8c6ff11352f607dec139013525d88434aaffd11c
Christopher Luey <41104962+chrisluey@users.noreply.github.com>
2022-01-15 15:11:07 -0800

    element-plus actually added

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,5 +2,20 @@
   <div>
     <h1>Profile</h1>
     <p>This page no longer exists or was moved to another location.</p>
+    <el-button type="primary"> Element UI </el-button>
+    <div class = "left-panel">
+    </div>
+    <div class = "profile-panel">
+
+        </div>
   </div>
 </template>
+
+<style scoped>
+.left-panel {
+    width: 200px;
+    position: fixed;
+    background-color: #edebe8;
+    height: 100%;
+}
+</style>
