335b990c19468bccff118d6ccaa54223175e1871
Austin <438157550steven@gmail.com>
2022-01-15 18:22:30 -0800

    navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <h1>Welcome</h1>
+    <nav>
+      <el-button><navbar></navbar></el-button>
+    </nav>
     <p>This page no longer exists or was moved to another location.</p>
   </div>
 </template>
+
+
+
+<script>
+import navbar from "../components/navbar.vue"
+import Navbar from "../components/navbar.vue"
+export default{
+    name: "Home",
+    componets: {
+        "navbar": navbar
+    },
+    components: { Navbar }
+}
+</script>
